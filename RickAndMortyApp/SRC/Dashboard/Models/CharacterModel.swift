fa38a69d582c77d177456171a9ed804f9fa31d60
Juan <juan.gonzalez@somosetica.com>
2025-06-13 01:04:15 -0300

    Se crea Modelos y Entidades de Character, Se hace modificaciones en API Client , Se agrega Librerias con SPM

--- RickAndMortyApp/SRC/Dashboard/Models/CharacterModel.swift
+++ RickAndMortyApp/SRC/Dashboard/Models/CharacterModel.swift
@@ -2,6 +2,51 @@
 //  CharacterModel.swift
 //  RickAndMortyApp
 //
-//  Created by Juan Gonzalez on 2025-06-12.
+//  Created by Juan Gonzalez on 2025-06-13.
 //
 
+import ObjectMapper
+import Foundation
+
+class CharacterModel: Mappable {
+    var id: Int?
+    var name: String?
+    var status: String?
+    var species: String?
+    var type: String?
+    var gender: String?
+    var originName: String?
+    var locationName: String?
+    var image: String?
+    var episode: [String] = []
+
+    required init?(map: Map) {}
+
+    func mapping(map: Map) {
+        id           <- map["id"]
+        name         <- map["name"]
+        status       <- map["status"]
+        species      <- map["species"]
+        type         <- map["type"]
+        gender       <- map["gender"]
+        originName   <- map["origin.name"]
+        locationName <- map["location.name"]
+        image        <- map["image"]
+        episode      <- map["episode"]
+    }
+
+    func toEntity() -> CharacterEntity {
+        return CharacterEntity(
+            id: id,
+            name: name,
+            status: status,
+            species: species,
+            type: type,
+            gender: gender,
+            origin: originName,
+            location: locationName,
+            imageURL: image,
+            episodeCount: episode.count
+        )
+    }
+}
